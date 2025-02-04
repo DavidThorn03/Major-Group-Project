722e57569438ad63bbe081cf02dbf138c71c2107
nikiandy <B00152278@mytudublin.ie>
2025-02-04 21:51:46 +0000

    l

--- ThreadUD/app/services/getPost.js
+++ ThreadUD/app/services/getPost.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { API_URL } from "../constants/apiConfig";
 
-export const getPostsByThread = async (threadId) => {
-  console.log(`Fetching posts for thread: ${threadId}`);
+export const getPosts = async () => {
+  console.log("Fetching posts...");
   try {
-    const response = await axios.get(`${API_URL}/thread/${threadId}/posts`);
+    const response = await axios.get(`${API_URL}/post`);
     return response.data;
   } catch (error) {
     console.error("Error fetching posts:", error);
